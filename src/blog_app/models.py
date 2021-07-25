b2d8cdd322d8edb4aeb8cbcf6703ad31ebb5a7a6
Ata-92 <atakal92@gmail.com>
2021-07-25 19:43:44 +0300

    Add one-to-many Comment model

--- src/blog_app/models.py
+++ src/blog_app/models.py
@@ -8,3 +8,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="profile_images", blank=True)
     bio = models.TextField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
