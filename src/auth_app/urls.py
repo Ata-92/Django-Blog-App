0a5c49bc0d53e555b69c76738eec542d48d61842
Ata-92 <atakal92@gmail.com>
2021-07-27 14:06:24 +0300

    Add password reset urls and templates

--- src/auth_app/urls.py
+++ src/auth_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.views.generic.base import TemplateView
 from auth_app.views import home_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", home_view, name="home"),
-    path("login/", auth_views.LoginView.as_view(template_name="auth_app/login.html"), name="login")
+    path("login/", auth_views.LoginView.as_view(template_name="auth_app/login.html"), name="login"),
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="auth_app/password_reset.html"), name="password_reset"),
+    path("password_reset_done/", auth_views.PasswordResetDoneView.as_view(template_name="auth_app/password_reset_done.html"), name="password_reset_done"),
 ]
