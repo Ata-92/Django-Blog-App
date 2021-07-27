4bbd06dfb58997aa989535055b5a9306b9dc9770
Ata-92 <atakal92@gmail.com>
2021-07-27 18:15:54 +0300

    Change register form ordering and style it

--- src/auth_app/forms.py
+++ src/auth_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db.models import fields
 
 # class LoginForm(forms.ModelForm):
 #     class Meta():
@@ -9,3 +10,6 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
+    class Meta():
+        model = User
+        fields = ("username", "email")
