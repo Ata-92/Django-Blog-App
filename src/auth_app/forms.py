b9372848cabd9cdc7d6ae2c8283bc7d4ee4a4d25
Ata-92 <atakal92@gmail.com>
2021-07-27 16:16:26 +0300

    Add a register form

--- src/auth_app/forms.py
+++ src/auth_app/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import fields
 
 # class LoginForm(forms.ModelForm):
 #     class Meta():
 #         model = User
 #         fields = ("username", "password")
+
+class RegisterForm(forms.Form, UserCreationForm):
+    email = forms.EmailField()
