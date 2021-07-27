cba67bfa910aece0e19ac212eedbe80fd703423f
Ata-92 <atakal92@gmail.com>
2021-07-27 17:30:15 +0300

    Add register url

--- src/auth_app/urls.py
+++ src/auth_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from auth_app.views import home_view
+from auth_app.views import home_view, register_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="auth_app/login.html"), name="login"),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="auth_app/password_reset.html"), name="password_reset"),
     path("password_reset_done/", auth_views.PasswordResetDoneView.as_view(template_name="auth_app/password_reset_done.html"), name="password_reset_done"),
+    path("register/", register_view, name="register")
 ]
