827f95a32c1ee8c44a405c929fa7f19adebd0863
Ata-92 <atakal92@gmail.com>
2021-07-27 21:08:16 +0300

    Add post app models to admin page

--- src/post_app/admin.py
+++ src/post_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from post_app.models import Category, Comment, Like, Post, PostView
+
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(PostView)
