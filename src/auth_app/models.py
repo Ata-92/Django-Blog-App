5f6dd84cb062773521c7426caf9ab7b837a6c14f
Ata-92 <atakal92@gmail.com>
2021-07-28 14:42:35 +0300

    Change Profile model field options

--- src/auth_app/models.py
+++ src/auth_app/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profile_images", blank=True)
-    bio = models.TextField()
+    image = models.ImageField(upload_to="profile_images")
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.user.username
