7403f22233efd086d82a9fcb6e95ff9421815302
Ata-92 <atakal92@gmail.com>
2021-07-28 23:26:02 +0300

    Add initial newpost view

--- src/post_app/urls.py
+++ src/post_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from post_app.views import newpost_view
 
 urlpatterns = [
     path("newpost/", newpost_view, name="newpost")
--- src/post_app/views.py
+++ src/post_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from post_app.forms import PostForm
+from django.contrib import messages
 
 # Create your views here.
+
+def newpost_view(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Post created successfully")
+        return redirect("home")
+    return render(request, "post_app/newpost.html", {"form": form})
