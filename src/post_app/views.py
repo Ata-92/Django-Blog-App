ba973dfb36503a1d088701a1e0667497d2e6b1f9
Ata-92 <atakal92@gmail.com>
2021-07-29 17:04:37 +0300

    Complete newpost view

--- src/post_app/views.py
+++ src/post_app/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import redirect, render
-from post_app.forms import PostForm
+from post_app.forms import CategoryForm, PostForm
 from django.contrib import messages
+
+from post_app.models import Category, Post
 
 # Create your views here.
 
 def newpost_view(request):
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+    post_form = PostForm(request.POST or None, request.FILES or None)
+    category_form = CategoryForm(request.POST or None)
+    if post_form.is_valid() and category_form.is_valid():
+        category_data = category_form.cleaned_data["name"]
+        if Category.objects.filter(name=category_data):
+            category = Category.objects.get(name=category_data)
+        else:
+            category = category_form.save()
+
+        post = post_form.save(commit=False)
+        post.category = category
+        post.user = request.user
+        post.save()
         messages.success(request, "Post created successfully")
         return redirect("home")
-    return render(request, "post_app/newpost.html", {"form": form})
+    context = {
+        "post_form":post_form,
+        "category_form":category_form
+    }
+    return render(request, "post_app/newpost.html", context)
