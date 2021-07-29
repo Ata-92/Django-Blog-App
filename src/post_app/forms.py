04a7c5b90d81a63b93ffb703c1061a0c01ba4ea9
Ata-92 <atakal92@gmail.com>
2021-07-29 17:02:35 +0300

    Add CategoryForm and some widgets

--- src/post_app/forms.py
+++ src/post_app/forms.py
@@ -1,11 +1,28 @@
 from django import forms
-from post_app.models import Post
+from django.db.models import fields
+from django.forms import widgets
+from post_app.models import Category, Post
+
+class CategoryForm(forms.ModelForm):
+    class Meta():
+        model = Category
+        fields = ["name"]
+        labels = {"name": "Category"}
+        widgets = {
+            "name": forms.Select(attrs={"class": "form-select"})
+        }
 
 class PostForm(forms.ModelForm):
-    CATEGORIES = (("Option 1", "Frontend"), ("Option 2", "Backend"), ("Option 3", "Full Stack"))
-    STATUSES = (("Option 1", "Draft"), ("Option 2", "Published"))
-    category = forms.ChoiceField(choices=CATEGORIES)
-    status = forms.ChoiceField(choices=STATUSES)
+    STATUSES = (("Draft", "Draft"), ("Published", "Published"))
+    status = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-select"}), choices=STATUSES)
     class Meta():
+        # STATUSES = (("Draft", "Draft"), ("Published", "Published"))
         model = Post
-        exclude = ("user",)
+        exclude = ("user", "category", "slug")
+        # fields = ("title", "content", "image", "status")
+        # widgets = {
+        #     "title": forms.TextInput(attrs={"class": "form-control"}),
+        #     "content": forms.Textarea(attrs={"class": "form-control"}),
+        #     "image": forms.FileInput(attrs={"class": "form-control"}),
+        #     "status": forms.Select(choices=STATUSES, attrs={"class": "form-select"})
+        # }
