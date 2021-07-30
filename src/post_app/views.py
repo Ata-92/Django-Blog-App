58c74bc8527051e0fde60e9ef1f23c4fefecd0e7
Ata-92 <atakal92@gmail.com>
2021-07-31 01:08:13 +0300

    Remove CategoryForm from newpost view

--- src/post_app/views.py
+++ src/post_app/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import redirect, render
-from post_app.forms import CategoryForm, PostForm
+from django.shortcuts import get_object_or_404, redirect, render
+from post_app.forms import CommentForm, PostForm
 from django.contrib import messages
-from post_app.models import Category, Post
+from post_app.models import Category, Comment, Like, Post, PostView
 
 # Create your views here.
 
 def newpost_view(request):
     post_form = PostForm(request.POST or None, request.FILES or None)
-    category_form = CategoryForm(request.POST or None)
-    if post_form.is_valid() and category_form.is_valid():
-        category_data = category_form.cleaned_data["name"]
+    if post_form.is_valid():
+        category_data = post_form.cleaned_data["category"]
         if Category.objects.filter(name=category_data):
             category = Category.objects.get(name=category_data)
         else:
-            category = category_form.save()
+            category = Category.objects.create(name=category_data)
 
         post = post_form.save(commit=False)
         post.category = category
@@ -21,8 +20,8 @@ def newpost_view(request):
         post.save()
         messages.success(request, "Post created successfully")
         return redirect("home")
+
     context = {
-        "post_form":post_form,
-        "category_form":category_form
+        "post_form": post_form
     }
     return render(request, "post_app/newpost.html", context)
