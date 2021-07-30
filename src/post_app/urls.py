082e0c7bad49d51fa3ea8292d0d76efcecf5570a
Ata-92 <atakal92@gmail.com>
2021-07-31 02:39:09 +0300

    Add update url

--- src/post_app/urls.py
+++ src/post_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from post_app.views import details_view, newpost_view
+from post_app.views import details_view, newpost_view, update_view
 
 urlpatterns = [
     path("newpost/", newpost_view, name="newpost"),
     path("<str:slug>/", details_view, name="details"),
-    # path("<str:slug>/update", update_view, name="update"),
+    path("<str:slug>/update", update_view, name="update"),
     # path("<str:slug>/delete/", delete_view, name="delete")
 ]
