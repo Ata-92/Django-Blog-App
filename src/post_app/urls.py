447df1a7a2373c48d5899d7a60d5bbe64a0219bd
Ata-92 <atakal92@gmail.com>
2021-07-31 03:09:00 +0300

    Add detail url to urls and template

--- src/post_app/urls.py
+++ src/post_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from post_app.views import details_view, newpost_view, update_view
+from post_app.views import delete_view, details_view, newpost_view, update_view
 
 urlpatterns = [
     path("newpost/", newpost_view, name="newpost"),
     path("<str:slug>/", details_view, name="details"),
-    path("<str:slug>/update", update_view, name="update"),
-    # path("<str:slug>/delete/", delete_view, name="delete")
+    path("<str:slug>/update/", update_view, name="update"),
+    path("<str:slug>/delete/", delete_view, name="delete")
 ]
