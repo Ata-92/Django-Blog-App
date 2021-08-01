b911937a15aaf4de48663bd829c184a7de325967
Ata-92 <atakal92@gmail.com>
2021-08-02 01:19:13 +0300

    Add urls for password reset

--- src/auth_app/urls.py
+++ src/auth_app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="auth_app/login.html"), name="login"),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="auth_app/password_reset.html"), name="password_reset"),
     path("password_reset_done/", auth_views.PasswordResetDoneView.as_view(template_name="auth_app/password_reset_done.html"), name="password_reset_done"),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="auth_app/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="auth_app/password_reset_complete.html"), name='password_reset_complete'),
     path("register/", register_view, name="register"),
     path("inactive/", register_view, name="inactive"),
     path("logout/", logout_view, name="logout"),
