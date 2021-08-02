a220f38d7a532684817cd96c6ffae2708d276c01
Ata-92 <atakal92@gmail.com>
2021-08-02 03:29:04 +0300

    Add category urls

--- src/post_app/urls.py
+++ src/post_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from post_app.views import delete_view, details_view, newpost_view, update_view
+from post_app.views import backend_view, delete_view, details_view, frontend_view, full_stack_view, newpost_view, update_view
 
 urlpatterns = [
     path("newpost/", newpost_view, name="newpost"),
+    path("frontend/", frontend_view, name="frontend"),
+    path("backend/", backend_view, name="backend"),
+    path("full_stack/", full_stack_view, name="full_stack"),
     path("<str:slug>/", details_view, name="details"),
     path("<str:slug>/update/", update_view, name="update"),
-    path("<str:slug>/delete/", delete_view, name="delete")
+    path("<str:slug>/delete/", delete_view, name="delete"),
 ]
