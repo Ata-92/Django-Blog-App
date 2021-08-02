de37acbc1eae30ea3313c6bad0a15a54c3ae62b1
Ata-92 <atakal92@gmail.com>
2021-08-02 03:38:21 +0300

    Make email host hidden

--- src/blog_project/settings.py
+++ src/blog_project/settings.py
@@ -113,8 +113,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'atakal92@gmail.com'
-EMAIL_HOST_PASSWORD = 'a;:b123cA'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
